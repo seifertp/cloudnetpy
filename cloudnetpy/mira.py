3afc6e95a275ab3fb823aa31d29db21658588550
seifertp <seifert@tropos.de>
2019-03-15 09:51:34 +0100

    changed from total to hydrometeor-only momemts

--- cloudnetpy/mira.py
+++ cloudnetpy/mira.py
@@ -17,11 +17,11 @@ class Mira(DataSource):
             keys are: 'name'.
 
     """
-    keymap = {'Zg': 'Ze',
-              'VELg': 'v',
-              'RMSg': 'width',
-              'LDRg': 'ldr',
-              'SNRg': 'SNR'}
+    keymap = {'Z': 'Ze',
+              'VEL': 'v',
+              'RMS': 'width',
+              'LDR': 'ldr',
+              'SNR': 'SNR'}
 
     def __init__(self, raw_radar_file, site_properties):
         super().__init__(raw_radar_file)
